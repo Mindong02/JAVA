966ccee1f788de6e19f9942b60566d3a6a97b43d
kkwkd <kkwkd@Dean>
2026-03-01 18:46:09 +0900

    string

---
+++ data/string.java
@@ -0,0 +1,11 @@
+public class string {
+	public static void main(String[] args) {
+		System.out.println("Hello world!"); //string
+		System.out.println("hello world!");// char
+		System.out.println('H'+'I');
+		System.out.println("Hello " + "world!");
+		System.out.println("hello \nworld!");
+		System.out.println("hello \"world\"");
+	}
+}
+ 
