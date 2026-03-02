e5a69839a5b4600894808c762717f066931b0bcb
kkwkd <kkwkd@Dean>
2026-03-02 21:21:22 +0900

    string

---
+++ data/StringOperation.java
@@ -0,0 +1,7 @@
+public class StringOperation {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("hellow world".length());
+		System.out.println("hello, [name]... bye.".replace("[name]", "doean"));
+	}
+}
--- data/string.java
+++ data/string.java
@@ -1,11 +1,13 @@
 public class string {
+	
 	public static void main(String[] args) {
-		System.out.println("Hello world!"); //string
-		System.out.println("hello world!");// char
-		System.out.println('H'+'I');
+		System.out.println("Hello world!"); //string 문자열
+		System.out.println("hello world!");// char 문자
+		System.out.println('H'+'I'); // 문자열 아
 		System.out.println("Hello " + "world!");
 		System.out.println("hello \nworld!");
 		System.out.println("hello \"world\"");
+		System.out.println("hell\n");
 	}
 }
- 
+
