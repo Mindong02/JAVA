f2f9dc2db910c463f9b982d0f5b483881c1078e1
kkwkd <kkwkd@Dean>
2026-03-02 22:04:42 +0900

    programming

---
+++ Programming/OkJavaGoHome.java
@@ -0,0 +1,25 @@
+import org.opentutorials.iot.Elevator;
+import org.opentutorials.iot.Lighting;
+import org.opentutorials.iot.Security;
+
+public class OkJavaGoHome {
+
+	public static void main(String[] args) {
+
+		// ELevator Call
+		Elevator myElevator = new Elevator("JARA APT 507");
+		myElevator.callForUp(1);
+				
+		// Security off
+		Security mySecurity = new Security("JAVA APT 507");
+		mySecurity.off();
+		
+		// Light on
+		Lighting hallLamp = new Lighting("JAVA APT 507 / Hall Lamp");
+		hallLamp.on();
+		
+		Lighting floorLamp = new Lighting("JAVA APT 507 / Hall Lamp");
+		floorLamp.on();		
+	}
+
+}
