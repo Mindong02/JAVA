9e0bdff9453d785fe87ce2f26086810ac69a8adc
kkwkd <kkwkd@Dean>
2026-03-02 21:47:37 +0900

    프로그램이란 무엇인가

---
+++ Programming/Program.java
@@ -0,0 +1,12 @@
+
+public class Program {
+
+	public static void main(String[] args) {
+		System.out.println(1);
+		System.out.println(2);
+		System.out.println(3);
+	}
+	
+	//프로그램은 순차적으로 일을 실행할 수 있다.
+	//자동화가 가능하다.
+}
